1b710a99f77cb412d9b461f99d0c047d2be55b7d
aneeshpm <aneesh.pm@naicoits.com>
2021-11-07 15:03:51 +0530

    cors added

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
 const client = require("./database");
+const cors = require("cors");
 // import route
 const authRoute = require("./routes/auth");
 // const postRoute = require("./routes/posts");
-
+const whitelist = [
+  "http://localhost:8000",
+  "https://stock-detail.herokuapp.com",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 // route middleware
 app.use("/api/user", authRoute);
 // app.use("/api/home", postRoute);
 
-app.listen(8000, () => console.log("Server is Up and running"));
+app.listen(process.env.PORT || 8000, () =>
+  console.log("Server is Up and running")
+);
