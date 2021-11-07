e7c1ca16a5619eb85229030008c9fd1ea872675b
aneeshpm <aneesh.pm@naicoits.com>
2021-11-07 15:05:52 +0530

    database changes

--- database.js
+++ database.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const client = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = client;
